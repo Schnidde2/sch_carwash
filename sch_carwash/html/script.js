2720b401ce7d605fe9b2937062bd7b87f4a2346d
RealDebugg <52179011+RealDebugg@users.noreply.github.com>
2022-07-26 00:28:02 +0200

    optimizations

--- sch_carwash/html/script.js
+++ sch_carwash/html/script.js
@@ -1,88 +1,60 @@
 $(function () {
     window.addEventListener("message", function (event) {
-        if (event.data.action == "setStandard") {
-            setAnzahl(event.data.pr)
-            $("#preis").show();
-        };
-        if (event.data.action == "setPremium") {
-            setAnzahle(event.data.pr)
-            $("#preis2").show();
-        };
-        if (event.data.action == "showUI") {
-            $("#UI").show();
-        };
-        if (event.data.action == "hideUI") {
-            $("#UI").hide();
-        };
-        if (event.data.action == "showLogo") {
-            $("#logo").show();
-        };
-        if (event.data.action == "hideLogo") {
-            $("#logo").hide();
-        };
-        if (event.data.action == "showNormal") {
-            $("#Aufzaehlungen2").show();
-            $("#Aufzaehlungen3").hide();
-        };
-        if (event.data.action == "showPremium") {
-            $("#Aufzaehlungen3").show();
-            $("#Aufzaehlungen2").hide();
-        };
+        switch (event.data.action) {
+            case "SetValues":
+                setAnzahle(event.data.sp, event.data.pp);
+                break;
+            case "UI":
+                switch (event.data.bool) {
+                    case true:
+                        $("#UI").show();
+                        break;
+                    case false:
+                        $("#UI").hide();
+                        break;
+                    default:
+                        console.error("non-specified argument received");
+                }
+                break;
+            case "showLogo":
+                $("#logo").show();
+                break;
+            case "ShowDetails":
+                switch (event.data.type) {
+                    case "normal":
+                        $("#Aufzaehlungen2").show();
+                        $("#Aufzaehlungen3").hide();
+                        break;
+                    case "premium":
+                        $("#Aufzaehlungen3").show();
+                        $("#Aufzaehlungen2").hide();
+                        break;
+                }
+                break;
+            default:
+                console.error("non-specified message received");
+                break;
+        }
     });
-});
-
-function display(bool) {
-    if (bool) {
-        $("#UI").show();
-    } else {
-        $("#UI").hide();
-    }
-    $.post('http://sch_carwash/exit', JSON.stringify({}));
-            return
-}
-
-function WashVehicle() {
-        $.post('https://sch_carwash/WashVehicle', JSON.stringify({}));
-}
-
-function WashHalfVehicle() {
-        $.post('https://sch_carwash/WashHalfVehicle', JSON.stringify({}));
-}
-
-$(function () {
-    function display(bool) {
-        if (bool) {
-            $("#UI").show();
-        } else {
-            $("#UI").hide();
-        }
-    }
-
-    display(false)
-
-    window.addEventListener('message', function(event) {
-        var item = event.data;
-        if (item.type === "ui") {
-            if (item.status == true) {
-                display(true)
-            } else {
-                display(false)
-            }
-        }
-    })
 
     document.onkeyup = function (data) {
         if (data.which == 27) {
+            $("#UI").hide();
             $.post('http://sch_carwash/exit', JSON.stringify({}));
             return
         }
     };
 });
 
-function setAnzahl(anzahl) {
-    document.getElementById("preis").innerHTML = new Intl.NumberFormat('de-DE').format(anzahl) + "$";
+function WashVehicle() {
+    $.post('https://sch_carwash/WashVehicle', JSON.stringify({}));
 }
 
-function setAnzahle(anzahl) {
-    document.getElementById("preis2").innerHTML = new Intl.NumberFormat('de-DE').format(anzahl) + "$";
+function WashHalfVehicle() {
+    $.post('https://sch_carwash/WashHalfVehicle', JSON.stringify({}));
+}
+
+function setAnzahle(sp, pp) {
+    $("#preis").html(new Intl.NumberFormat('de-DE').format(sp) + "$");
+    $("#preis2").html(new Intl.NumberFormat('de-DE').format(pp) + "$");
 }
